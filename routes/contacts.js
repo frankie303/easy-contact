d13f0dbbbb24f05e50ec20231a7288bdad3289bd
mert <uzeyirmertciflikli@gmail.com>
2020-05-28 10:16:29 +0200

    auth, contacts and users routes added

---
+++ routes/contacts.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const router = express.Router();
+
+// @route   GET api/contacts
+// @desc    Get all users contacts
+// @access  Private
+router.get('/', (req, res) => {
+  res.send('Get all contacts');
+});
+
+// @route   POST api/contacts
+// @desc    Update contact
+// @access  Private
+router.put('/id', (req, res) => {
+  res.send('Add contact');
+});
+
+module.exports = router;
+
+
+// @route   PUT api/contacts(:id)
+// @desc    Update contact
+// @access  Private
+router.put('/:id', (req, res) => {
+  res.send('Update contact');
+});
+
+module.exports = router;
+
+// @route   DELETE api/contacts(:id)
+// @desc    Update contact
+// @access  Private
+router.delete('/:id', (req, res) => {
+  res.send('Delete contact');
+});
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.json({msg: 'Welcome to the EasyKeeper API...'}));
+
+// define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
