e9e753e9bf8b1d136ee0221a442979bc0586b2ce
mert <uzeyirmertciflikli@gmail.com>
2020-06-07 15:13:00 +0200

    jwt set up

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const { check, validationResult } = require('express-validator/check');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 
 const User = require('../models/User');
@@ -44,8 +46,18 @@ async (req, res) => {
 
     await user.save();
 
-    res.send('User saved');
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
 
+    jwt.sign(payload, config.get('jwtSecret'), {
+      expiresIn: 360000
+    }, (err, token) => {
+      if(err)  throw err;
+      res.json({ token });
+    });
   } catch (err) {
     console.error(err.massage);
     res.status(500).send('Server error')
