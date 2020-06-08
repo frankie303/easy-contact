8bbe4c613605c08fafdcdd7105d612aeb0c6dc4e
mert <uzeyirmertciflikli@gmail.com>
2020-06-08 11:21:07 +0200

    contact model added, contacts route updated

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,24 @@
 const express = require('express');
-
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
+const auth = require('../middleware/auth');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route   GET api/contacts
 // @desc    Get all users contacts
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get all contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   POST api/contacts
@@ -16,17 +28,12 @@ router.put('/id', (req, res) => {
   res.send('Add contact');
 });
 
-module.exports = router;
-
-
 // @route   PUT api/contacts/:id
 // @desc    Update contact
 // @access  Private
 router.put('/:id', (req, res) => {
   res.send('Update contact');
 });
-
-module.exports = router;
 
 // @route   DELETE api/contacts/:id
 // @desc    Delete contact
