82cea28bd2b457092144480fe477e89eea1c065a
mert <uzeyirmertciflikli@gmail.com>
2020-06-14 07:51:27 +0200

    Navbar links added and page components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-      My App
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+
+          </Switch>
+        </div>
+      </>
+    </Router>
   );
 }
 
