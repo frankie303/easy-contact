b3bfd21f0c0de74e7b7c4739e6bf01519cee8bfb
mert <uzeyirmertciflikli@gmail.com>
2020-06-14 06:10:21 +0200

    navbar addded

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar'
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
       My App
     </div>
   );
 }
 
 export default App;
+
