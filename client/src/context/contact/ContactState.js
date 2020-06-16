783f43cb6ea14bfdd3e161d8a4c0689d01f94eff
mert <uzeyirmertciflikli@gmail.com>
2020-06-16 19:23:30 +0200

    delete contact added

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -48,6 +48,9 @@ const ContactState = props => {
   };
 
   //delete contact
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id});
+  };
 
   //set current contact
 
@@ -63,7 +66,8 @@ const ContactState = props => {
     <ContactContext.Provider
     value= {{
       contacts: state.contacts,
-      addContact
+      addContact,
+      deleteContact
     }}>
       { props.children }
     </ContactContext.Provider>
