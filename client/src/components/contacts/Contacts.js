49831088985d4a931a647d3c069402ec585a488f
mert <uzeyirmertciflikli@gmail.com>
2020-06-22 11:27:19 +0200

    contact filter added and some prettier auto-format added

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,18 +1,27 @@
-import React, { useContext } from 'react'
-import ContactContext from '../../context/contact/contactContext'
-import ContactItem from './ContactItem'
+import React, { useContext } from 'react';
+import ContactContext from '../../context/contact/contactContext';
+import ContactItem from './ContactItem';
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
 
   return (
     <>
-      {contacts.map(contact => 
-      <ContactItem key={contact.id} contact={contact}>{contact.name}</ContactItem>)}
+      {filtered !== null
+        ? filtered.map((contact) => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))
+        : contacts.map((contact) => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))}
     </>
-  )
-}
+  );
+};
 
 export default Contacts;
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -5,49 +5,52 @@ import {
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
-  CLEAR_FILTER
+  CLEAR_FILTER,
 } from '../types';
 
 export default (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [...state.contacts, action.payload]
+        contacts: [...state.contacts, action.payload],
       };
     case UPDATE_CONTACT:
       return {
         ...state,
-        contacts: state.contacts.map(contact =>
-           contact.id === action.payload.id ? action.payload : contact)
-      }
+        contacts: state.contacts.map((contact) =>
+          contact.id === action.payload.id ? action.payload : contact
+        ),
+      };
     case DELETE_CONTACT:
       return {
         ...state,
-        contacts: state.contacts.filter(contact => contact.id !== action.payload)
-      }
+        contacts: state.contacts.filter(
+          (contact) => contact.id !== action.payload
+        ),
+      };
     case SET_CURRENT:
       return {
         ...state,
-        current: action.payload
-      }
+        current: action.payload,
+      };
     case CLEAR_CURRENT:
-    return {
-      ...state,
-      current: null
-    };
+      return {
+        ...state,
+        current: null,
+      };
     case FILTER_CONTACTS:
       return {
         ...state,
-        filtered: state.contacts.filter(contact => {
-          const regex = new RegExp(`${action.payload}, 'gi`);
+        filtered: state.contacts.filter((contact) => {
+          const regex = new RegExp(`${action.payload}`, 'gi');
           return contact.name.match(regex) || contact.email.match(regex);
-        })
+        }),
       };
     case CLEAR_FILTER:
       return {
         ...state,
-        filtered: null
+        filtered: null,
       };
     default:
       return state;
