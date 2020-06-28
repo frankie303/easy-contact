174a47f7b2f9fc3a78fb118e6c019ef9a60c175e
mert <uzeyirmertciflikli@gmail.com>
2020-06-28 18:11:30 +0200

    alerts added to Context

--- client/src/App.js
+++ client/src/App.js
@@ -8,24 +8,27 @@ import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <>
-            <Navbar />
-            <div className='container'>
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
-              </Switch>
-            </div>
-          </>
-        </Router>
+        <AlertState>
+          <Router>
+            <>
+              <Navbar />
+              <div className='container'>
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
