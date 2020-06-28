a5c999bba20c2d875398cfc1d1f7eb9db9fa57e9
mert <uzeyirmertciflikli@gmail.com>
2020-06-28 17:50:06 +0200

    register and login components added

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ title, icon }) => {
   return (
-    <div className="navbar bg-primary">
+    <div className='navbar bg-primary'>
       <h1>
         <i className={icon} /> {title}
       </h1>
@@ -15,19 +15,25 @@ const Navbar = ({ title, icon }) => {
         <li>
           <Link to='/about'>About</Link>
         </li>
+        <li>
+          <Link to='/register'>Register</Link>
+        </li>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
   icon: PropTypes.string
-}
+};
 
 Navbar.defaultProps = {
   title: 'Contact Keeper',
   icon: 'fas fa-id-card-alt'
-}
+};
 
-export default Navbar
+export default Navbar;
