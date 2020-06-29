03bd04ac0dd799db1c4f4d353807dcb3efae6987
mert <uzeyirmertciflikli@gmail.com>
2020-06-29 16:15:19 +0200

    load user and set token added

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,11 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alerts from './components/layout/Alerts';
 import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 const App = () => {
   return (
     <AuthState>
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import Contacts from '../contacts/Contacts'
-import ContactForm from '../contacts/ContactForm'
-import ContactFilter from '../contacts/ContactFilter'
+import React, { useContext, useEffect } from 'react';
+import Contacts from '../contacts/Contacts';
+import ContactForm from '../contacts/ContactForm';
+import ContactFilter from '../contacts/ContactFilter';
+import AuthContext from '../../context/auth/authContext';
 
 const Home = () => {
+  const authContext = useContext(AuthContext);
+
+  useEffect(() => {
+    authContext.loadUser();
+    // eslint-disable-next-line
+  }, []);
+
   return (
-    <div className="grid-2">
+    <div className='grid-2'>
       <div>
         <ContactForm />
       </div>
@@ -14,7 +22,7 @@ const Home = () => {
         <Contacts />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
