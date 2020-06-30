9d93f5a1d36482f51682e3194742920b5cd2cbdd
mert <uzeyirmertciflikli@gmail.com>
2020-06-30 11:45:44 +0200

    delete contact added

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,50 +1,58 @@
-import React, { useContext } from 'react'
-import PropTypes from 'prop-types'
-import ContactContext from '../../context/contact/contactContext'
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import ContactContext from '../../context/contact/contactContext';
 
 const ContactItem = ({ contact }) => {
   const contactContext = useContext(ContactContext);
-  const { deleteContact, setCurrent, clearCurrent } = contactContext;
+  const { deleteContact, setCurrent, clearCurrent } = contactContext;
 
-  const { id, name, email, phone, type } = contact;
+  const { _id, name, email, phone, type } = contact;
 
   const onDelete = () => {
-    deleteContact(id);
+    deleteContact(_id);
     clearCurrent();
-
-  }
+  };
   return (
     <div className='card bg-light'>
-    <h3 className="text-primary text-left">
-      {name}{' '} 
-      <span
-        className={'badge ' + (type === 'professional' ? 
-        'badge-success' : 'badge-primary' )
-        }
-      >
-        {type.charAt(0).toUpperCase() + type.slice(1)}
-      </span>
-    </h3>
-    <ul className="list">
-      {email && (<li>
-        <i className="fas fa-envelope-open"> {email}
-        </i>
-      </li>)}
-      {phone && (<li>
-        <i className="fas fa-phone"> {phone}
-        </i>
-      </li>)}
-    </ul>
-    <p>
-      <button className="btn btn-dark btn-sm" onClick={() => setCurrent(contact)}>Edit</button>
-      <button className="btn btn-danger btn-sm" onClick={onDelete}>Delete</button>
-
-    </p>
+      <h3 className='text-primary text-left'>
+        {name}{' '}
+        <span
+          className={
+            'badge ' +
+            (type === 'professional' ? 'badge-success' : 'badge-primary')
+          }
+        >
+          {type.charAt(0).toUpperCase() + type.slice(1)}
+        </span>
+      </h3>
+      <ul className='list'>
+        {email && (
+          <li>
+            <i className='fas fa-envelope-open'> {email}</i>
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className='fas fa-phone'> {phone}</i>
+          </li>
+        )}
+      </ul>
+      <p>
+        <button
+          className='btn btn-dark btn-sm'
+          onClick={() => setCurrent(contact)}
+        >
+          Edit
+        </button>
+        <button className='btn btn-danger btn-sm' onClick={onDelete}>
+          Delete
+        </button>
+      </p>
     </div>
-  )
-}
+  );
+};
 
 ContactItem.propTypes = {
   contact: PropTypes.object.isRequired
-}
-export default ContactItem
+};
+export default ContactItem;
