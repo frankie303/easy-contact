2dd4c354f1d79516058ebc8cc177d572326b4c84
mert <uzeyirmertciflikli@gmail.com>
2020-07-01 11:21:25 +0200

    footer github link is fixed

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -5,7 +5,11 @@ const Footer = () => {
     <div>
       <p className='text-center'>
         Made with <i className='far fa-heart' /> in Berlin{' '}
-        <a href='https://github.com/frankie303' target='_blank'>
+        <a
+          href='https://github.com/frankie303'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
           © 2020 Mert Ciflikli
         </a>
       </p>
